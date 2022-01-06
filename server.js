bcd7137a866895b15e975f3e1b4723f0e03851dd
aldi <aldilukman007@gmail.com>
2022-01-07 05:56:44 +0700

    update port server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./app/routes/newscampaign.routes")(app);
 require("./app/routes/rule.routes")(app);
 require("./app/routes/campaignusermessage.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
